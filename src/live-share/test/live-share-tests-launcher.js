a34f0acfec5117e0f8cf85fd3dc44bc85b6cf4df
Oleg Solomka <legomushroom@gmail.com>
2019-05-31 12:46:22 -0700

    [test] pass the current env to the live share tests

--- src/live-share/test/live-share-tests-launcher.js
+++ src/live-share/test/live-share-tests-launcher.js
@@ -7,6 +7,7 @@ const testsPath = path.join(process.cwd(), 'out', 'live-share', 'test');
 
 const child = fork('node_modules/vscode/bin/test', [], {
   env: {
+    ...process.env,
     CODE_TESTS_PATH: testsPath
   },
   cwd: process.cwd()
