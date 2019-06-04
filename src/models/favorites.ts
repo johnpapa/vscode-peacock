592ce7112a560daeb325cf14c7b677201438254e
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-06-05 00:46:37 +0200

    Add "Go Cyan" favorite color
    The color was taken from the https://golang.org/ favicon
    which represents the Go mascot. This was done to provide a color
    that's easy to distinguish from other colors such as React Blue.

--- src/models/favorites.ts
+++ src/models/favorites.ts
@@ -4,6 +4,7 @@ export const starterSetOfFavorites = [
   { name: 'Azure Blue', value: '#007fff' },
   { name: 'C# Purple', value: '#68217A' },
   { name: 'Gatsby Purple', value: '#639' },
+  { name: 'Go Cyan', value: '#76e1fe' },
   { name: 'Java Blue-Gray', value: '#557c9b' },
   { name: 'JavaScript Yellow', value: '#f9e64f' },
   { name: 'Mandalorian Blue', value: '#1857a4' },
