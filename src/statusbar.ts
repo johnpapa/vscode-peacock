2d61ff2bd88e5c7aa74408d8ff4c8d7e300d950f
John Papa <johnpapa@users.noreply.github.com>
2019-07-24 09:37:49 -0400

    statusbar changes

--- src/statusbar.ts
+++ src/statusbar.ts
@@ -1,28 +1,27 @@
 import { StatusBarAlignment, window, StatusBarItem } from 'vscode';
 import { getShowColorInStatusBar } from './configuration';
+import { State } from './models';
 
-let statusBarItem: StatusBarItem;
+const _statusBarItem: StatusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
+
+export const getStatusBarItem = () => {
+  updateStatusBar();
+  return _statusBarItem;
+};
 
 export function clearStatusBar() {
-  createStatusBarItem();
-
-  statusBarItem.text = '';
-  statusBarItem.hide();
+  const sb = _statusBarItem;
+  // sb.text = '';
+  sb.hide();
 }
 
-export function updateStatusBar(text: string) {
+export function updateStatusBar() {
+  const sb = _statusBarItem;
   const show = getShowColorInStatusBar();
-  if (show && text) {
-    createStatusBarItem();
-    statusBarItem.text = `$(paintcan) ${text}`;
-    statusBarItem.show();
+  sb.text = `$(paintcan) ${State.recentColor}`;
+  if (show) {
+    sb.show();
   } else {
     clearStatusBar();
-  }
-}
-
-function createStatusBarItem() {
-  if (!statusBarItem) {
-    statusBarItem = window.createStatusBarItem(StatusBarAlignment.Left);
   }
 }
