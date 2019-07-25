5ae6861087ae23c1a43f3458c9f81735e951c826
John Papa <john@johnpapa.net>
2019-07-24 23:42:59 -0400

    fix live tests

--- src/live-share/test/suite/live-share.test.ts
+++ src/live-share/test/suite/live-share.test.ts
@@ -3,7 +3,7 @@ import sinon = require('sinon');
 import assert = require('assert');
 import * as vsls from 'vsls';
 
-import { IPeacockSettings, Commands, ColorSettings, timeout, azureBlue } from '../../../models';
+import { IPeacockSettings, Commands, ColorSettings, timeout } from '../../../models';
 import {
   setupTestSuite,
   teardownTestSuite,
@@ -14,53 +14,47 @@ import { executeCommand } from '../../../test/suite/lib/constants';
 import { LiveShareCommands, LiveShareSettings } from '../../enums';
 import {
   getPeacockWorkspaceConfig,
-  getLiveShareColor,
   updateLiveShareColor,
+  getLiveShareColor,
 } from '../../../configuration';
 
 suite('Live Share Integration', () => {
   let originalValues = <IPeacockSettings>{};
-  let extensionContext: vscode.ExtensionContext | undefined;
 
   suiteSetup(async () => await setupTestSuite(originalValues));
   suiteTeardown(async () => await teardownTestSuite(originalValues));
   setup(async () => await setupTest());
 
-  setup(async () => {
-    extensionContext = await executeCommand<vscode.ExtensionContext>(
-      LiveShareCommands.changeColorOfLiveShareHost,
-    );
-  });
-
-  teardown(async () => {
-    await updateLiveShareColor(LiveShareSettings.VSLSShareColor, '');
-  });
-
   test('can set color setting for Live Share host', async () => {
     // Stub the async quick pick to return a response
-    const fakeResponse = `Azure Blue -> ${azureBlue}`;
-    const stub = await stubQickPick(fakeResponse);
-
+    const color = '#007fff';
+    const fakeResponse = `Azure Blue -> ${color}`;
+    const stub = await sinon
+      .stub(vscode.window, 'showQuickPick')
+      .returns(Promise.resolve<any>(fakeResponse));
+    await executeCommand(LiveShareCommands.changeColorOfLiveShareHost);
     const settingValue = getLiveShareColor(LiveShareSettings.VSLSShareColor) || '';
-
+    await updateLiveShareColor(LiveShareSettings.VSLSShareColor, '');
     stub.restore();
 
     assert(isValidColorInput(settingValue));
-    assert(settingValue === azureBlue);
+    assert(settingValue === color);
   });
 
   test('can set color setting for Live Share guest', async () => {
     // Stub the async quick pick to return a response
-    const fakeResponse = `Azure Blue -> ${azureBlue}`;
-    const stub = await stubQickPick(fakeResponse);
-
+    const color = '#007fff';
+    const fakeResponse = `Azure Blue -> ${color}`;
+    const stub = await sinon
+      .stub(vscode.window, 'showQuickPick')
+      .returns(Promise.resolve<any>(fakeResponse));
+    await executeCommand(LiveShareCommands.changeColorOfLiveShareGuest);
     const settingValue = getLiveShareColor(LiveShareSettings.VSLSJoinColor) || '';
     await updateLiveShareColor(LiveShareSettings.VSLSJoinColor, '');
-
     stub.restore();
 
     assert(isValidColorInput(settingValue));
-    assert(settingValue === azureBlue);
+    assert(settingValue === color);
   });
 
   test('Workspace color is updated when Live Share session is started.', async () => {
@@ -70,9 +64,12 @@ suite('Live Share Integration', () => {
       throw new Error('Live Share extension is not installed.');
     }
 
-    const fakeResponse = `Azure Blue -> ${azureBlue}`;
-    const stub = await stubQickPick(fakeResponse);
-
+    const color = '#007fff';
+    const fakeResponse = `Azure Blue -> ${color}`;
+    const stub = await sinon
+      .stub(vscode.window, 'showQuickPick')
+      .returns(Promise.resolve<any>(fakeResponse));
+    await executeCommand(LiveShareCommands.changeColorOfLiveShareHost);
     stub.restore();
 
     await vslsApi.share();
@@ -82,9 +79,10 @@ suite('Live Share Integration', () => {
     const value = config[ColorSettings.titleBar_activeBackground];
 
     await vslsApi.end();
+    await updateLiveShareColor(LiveShareSettings.VSLSShareColor, '');
 
     assert(isValidColorInput(value));
-    assert(value === azureBlue);
+    assert(value === color);
   });
 
   test('Workspace color is reverted when Live Share session is ended.', async () => {
@@ -94,9 +92,13 @@ suite('Live Share Integration', () => {
       throw new Error('Live Share extension is not installed.');
     }
 
-    const fakeResponse = `Azure Blue -> ${azureBlue}`;
-    const stub = await stubQickPick(fakeResponse);
+    const color = '#007fff';
+    const fakeResponse = `Azure Blue -> ${color}`;
+    const stub = await sinon
+      .stub(vscode.window, 'showQuickPick')
+      .returns(Promise.resolve<any>(fakeResponse));
 
+    await executeCommand(LiveShareCommands.changeColorOfLiveShareHost);
     stub.restore();
 
     await vslsApi.share();
@@ -107,7 +109,7 @@ suite('Live Share Integration', () => {
     let config = getPeacockWorkspaceConfig();
     const value = config[ColorSettings.titleBar_activeBackground];
 
-    // await updateLiveShareColor(LiveShareSettings.VSLSShareColor, '');
+    await updateLiveShareColor(LiveShareSettings.VSLSShareColor, '');
 
     assert(!isValidColorInput(value));
     assert(value == null);
@@ -122,18 +124,23 @@ suite('Live Share Integration', () => {
 
     await vslsApi.share();
 
-    const fakeResponse = `Azure Blue -> ${azureBlue}`;
-    const stub = await stubQickPick(fakeResponse);
+    const color = '#007fff';
+    const fakeResponse = `Azure Blue -> ${color}`;
+    const stub = await sinon
+      .stub(vscode.window, 'showQuickPick')
+      .returns(Promise.resolve<any>(fakeResponse));
 
+    await executeCommand(LiveShareCommands.changeColorOfLiveShareHost);
     stub.restore();
 
     let config = getPeacockWorkspaceConfig();
     const value = config[ColorSettings.titleBar_activeBackground];
 
     await vslsApi.end();
+    await updateLiveShareColor(LiveShareSettings.VSLSShareColor, '');
 
     assert(isValidColorInput(value));
-    assert(value === azureBlue);
+    assert(value === color);
   });
 
   test('Workspace color is immediately reflected when updated during Live Share session.', async () => {
@@ -142,9 +149,14 @@ suite('Live Share Integration', () => {
     if (!vslsApi) {
       throw new Error('Live Share extension is not installed.');
     }
-    const fakeResponse = `Azure Blue -> ${azureBlue}`;
-    const stub = await stubQickPick(fakeResponse);
 
+    const color = '#007fff';
+    const fakeResponse = `Azure Blue -> ${color}`;
+    const stub = await sinon
+      .stub(vscode.window, 'showQuickPick')
+      .returns(Promise.resolve<any>(fakeResponse));
+
+    await executeCommand(LiveShareCommands.changeColorOfLiveShareHost);
     stub.restore();
 
     await vslsApi.share();
@@ -153,9 +165,11 @@ suite('Live Share Integration', () => {
 
     const color2 = '#68217a';
     const fakeResponse2 = `C# Purple -> ${color2}`;
-    const stub2 = await stubQickPick(fakeResponse2);
+    const stub2 = await sinon
+      .stub(vscode.window, 'showQuickPick')
+      .returns(Promise.resolve<any>(fakeResponse2));
 
-    await executeCommand<vscode.ExtensionContext>(LiveShareCommands.changeColorOfLiveShareHost);
+    await executeCommand(LiveShareCommands.changeColorOfLiveShareHost);
     stub2.restore();
 
     await timeout(1000);
@@ -167,7 +181,7 @@ suite('Live Share Integration', () => {
     assert(value === color2);
 
     await vslsApi.end();
-    // await updateLiveShareColor(LiveShareSettings.VSLSShareColor, '');
+    await updateLiveShareColor(LiveShareSettings.VSLSShareColor, '');
   });
 
   test('Workspace color is reverted after Live Share session when updated the color multiple times.', async () => {
@@ -183,26 +197,33 @@ suite('Live Share Integration', () => {
 
     const color2 = '#68217a';
     const fakeResponse2 = `C# Purple -> ${color2}`;
-    const stub2 = await stubQickPick(fakeResponse2);
+    const stub2 = await sinon
+      .stub(vscode.window, 'showQuickPick')
+      .returns(Promise.resolve<any>(fakeResponse2));
 
+    await executeCommand(LiveShareCommands.changeColorOfLiveShareHost);
     stub2.restore();
 
     await timeout(1000);
 
     const color3 = '#b52e31';
     const fakeResponse3 = `Angular Red -> ${color3}`;
-    const stub3 = await stubQickPick(fakeResponse3);
+    const stub3 = await sinon
+      .stub(vscode.window, 'showQuickPick')
+      .returns(Promise.resolve<any>(fakeResponse3));
 
-    await executeCommand<vscode.ExtensionContext>(LiveShareCommands.changeColorOfLiveShareHost);
+    await executeCommand(LiveShareCommands.changeColorOfLiveShareHost);
     stub3.restore();
 
     await timeout(1000);
 
     const color4 = '#639';
     const fakeResponse4 = `Gatsby Purple -> ${color4}`;
-    const stub4 = await stubQickPick(fakeResponse4);
+    const stub4 = await sinon
+      .stub(vscode.window, 'showQuickPick')
+      .returns(Promise.resolve<any>(fakeResponse4));
 
-    await executeCommand<vscode.ExtensionContext>(LiveShareCommands.changeColorOfLiveShareHost);
+    await executeCommand(LiveShareCommands.changeColorOfLiveShareHost);
     stub4.restore();
 
     await timeout(1000);
@@ -217,7 +238,7 @@ suite('Live Share Integration', () => {
     assert(!isValidColorInput(value));
     assert(value == null);
 
-    // await updateLiveShareColor(LiveShareSettings.VSLSShareColor, '');
+    await updateLiveShareColor(LiveShareSettings.VSLSShareColor, '');
   });
 
   test('Workspace color is reverted to a preset color after Live Share session when updated the color multiple times.', async () => {
@@ -227,9 +248,11 @@ suite('Live Share Integration', () => {
       throw new Error('Live Share extension is not installed.');
     }
 
-    const startColor = azureBlue;
+    const startColor = '#007fff';
     const fakeResponse = `Azure Blue -> ${startColor}`;
-    const stub = await stubQickPick(fakeResponse);
+    const stub = await sinon
+      .stub(vscode.window, 'showQuickPick')
+      .returns(Promise.resolve<any>(fakeResponse));
 
     await executeCommand(Commands.changeColorToFavorite);
     stub.restore();
@@ -240,26 +263,33 @@ suite('Live Share Integration', () => {
 
     const color2 = '#68217a';
     const fakeResponse2 = `C# Purple -> ${color2}`;
-    const stub2 = await stubQickPick(fakeResponse2);
+    const stub2 = await sinon
+      .stub(vscode.window, 'showQuickPick')
+      .returns(Promise.resolve<any>(fakeResponse2));
 
+    await executeCommand(LiveShareCommands.changeColorOfLiveShareHost);
     stub2.restore();
 
     await timeout(1000);
 
     const color3 = '#b52e31';
-    const fakeResponse3 = `Abgular Red -> ${color3}`;
-    const stub3 = await stubQickPick(fakeResponse3);
+    const fakeResponse3 = `Angular Red -> ${color3}`;
+    const stub3 = await sinon
+      .stub(vscode.window, 'showQuickPick')
+      .returns(Promise.resolve<any>(fakeResponse3));
 
-    await executeCommand<vscode.ExtensionContext>(LiveShareCommands.changeColorOfLiveShareHost);
+    await executeCommand(LiveShareCommands.changeColorOfLiveShareHost);
     stub3.restore();
 
     await timeout(1000);
 
     const color4 = '#639';
     const fakeResponse4 = `Gatsby Purple -> ${color4}`;
-    const stub4 = await stubQickPick(fakeResponse4);
+    const stub4 = await sinon
+      .stub(vscode.window, 'showQuickPick')
+      .returns(Promise.resolve<any>(fakeResponse4));
 
-    await executeCommand<vscode.ExtensionContext>(LiveShareCommands.changeColorOfLiveShareHost);
+    await executeCommand(LiveShareCommands.changeColorOfLiveShareHost);
     stub4.restore();
 
     await timeout(1000);
@@ -274,13 +304,9 @@ suite('Live Share Integration', () => {
     assert(isValidColorInput(value));
     assert(value === startColor);
 
-    // await updateLiveShareColor(LiveShareSettings.VSLSShareColor, '');
+    await updateLiveShareColor(LiveShareSettings.VSLSShareColor, '');
   });
 });
 
-const stubQickPick = async (fakeResponse: string) => {
-  const stub = await sinon
-    .stub(vscode.window, 'showQuickPick')
-    .returns(Promise.resolve<any>(fakeResponse));
-  return stub;
-};
+const stubQuickPick = async (fakeResponse: string) =>
+  await sinon.stub(vscode.window, 'showQuickPick').returns(Promise.resolve<any>(fakeResponse));
