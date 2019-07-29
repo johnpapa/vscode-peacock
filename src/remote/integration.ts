f269aa1c07e24085a83e3ef2a3e2ecb65767d5e7
John Papa <john@johnpapa.net>
2019-07-29 10:50:48 -0400

    as per  egamma - The context peacock:remote was only used by the remote commands, since the remote commands got removed it is no longer necessary to set this context. As a consequence the function remoteExtensionsInstalled is also no longer needed.

--- src/remote/integration.ts
+++ src/remote/integration.ts
@@ -4,20 +4,20 @@ import { applyColor } from '../apply-color';
 import { State } from '../models';
 import { getPeacockRemoteColor, getPeacockColor } from '../configuration';
 
-function remoteExtensionsInstalled(): boolean {
-  let remoteExtensions = [
-    'ms-vscode-remote.remote-containers',
-    'ms-vscode-remote.remote-ssh',
-    'ms-vscode-remote.remote-wsl',
-  ];
-  return !!remoteExtensions.find(each => !!vscode.extensions.getExtension(each));
-}
+// function remoteExtensionsInstalled(): boolean {
+//   let remoteExtensions = [
+//     'ms-vscode-remote.remote-containers',
+//     'ms-vscode-remote.remote-ssh',
+//     'ms-vscode-remote.remote-wsl',
+//   ];
+//   return !!remoteExtensions.find(each => !!vscode.extensions.getExtension(each));
+// }
 
 export async function addRemoteIntegration(context: vscode.ExtensionContext) {
   State.extensionContext = context;
 
-  const remoteExtensions = remoteExtensionsInstalled();
-  await vscode.commands.executeCommand('setContext', 'peacock:remote', remoteExtensions);
+  // const remoteExtensions = remoteExtensionsInstalled();
+  // await vscode.commands.executeCommand('setContext', 'peacock:remote', remoteExtensions);
 
   // TODO: revisit this ... should we change colors for remote, non remote, or never, or both?
   if (vscode.env.remoteName) {
