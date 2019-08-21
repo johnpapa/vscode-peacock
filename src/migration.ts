a00dda743d1639d562ac6461f6669e998ce317ef
John Papa <johnpapa@users.noreply.github.com>
2019-08-21 11:47:56 -0400

    Migrate/only check workspace (#265)
    * refactored migration to its own file, as this will be deprecated in a later version
    
    * getWorkspaceColorIfExists --> get (and calculate for adjustments) a single color from the workspace settings if one exists. Ideal for migration when peacock.color did not exist in <v3.
    
    getOriginalColor --> if a non color is passed in, then pass the same thing back out. This protects against having a undefined or empty string passed in and #000 being passed out.
    
    getElementColors --> allows both vscode  workspaceconfiguration type AND ISettingsIndxer, since sometimes we have one or the other
    
    * v3.1.4
    
    * changelog for the fix

---
+++ src/migration.ts
@@ -0,0 +1,53 @@
+import { State, extensionShortName } from './models';
+import { getPeacockColor, getWorkspaceColorIfExists } from './configuration';
+import { updateColorSetting } from './apply-color';
+import { Logger } from './logging';
+
+export async function migrateFromMementoToSettingsAsNeeded() {
+  /**
+   * Version >2.5 of Peacock did not store the peacock color.
+   * Version 2.5 of Peacock stored the peacock color in a memento.
+   * Version 3 of Peacock stores the color in the settings.
+   * If peacock.color does not exist and we find either memento or a deried color,
+   * remove the memento, and write the apply the color to the settings.
+   *
+   * @deprecated since version 3.0.
+   * Will be deleted in version 4.0 and once v3.0 users have migrated
+   */
+  // If peacock.color exists, get out. Nothing to migrate.
+  const peacockColorExists = !!getPeacockColor();
+  if (peacockColorExists) {
+    Logger.info('Migration: Nothing to migrate, peacock.color exists.');
+    return;
+  }
+  // Check for the v2.5 memento
+  const peacockColorMementoName = `${extensionShortName}.peacockColor`;
+  const peacockColorMemento = State.extensionContext.workspaceState.get<string>(
+    peacockColorMementoName,
+  );
+  let derivedColor = undefined;
+  // The v2 memento is gone, so no need to migrate.
+  if (peacockColorMemento) {
+    Logger.info('Migration: Migrating from peacock memento to peacock.color setting.');
+    // Remove the v2 memento (it's ok if it doesnt exist)
+    await State.extensionContext.workspaceState.update(peacockColorMementoName, undefined);
+  } else {
+    /**
+     * There is no memento, so let's check if there is a
+     * color set in the 'old style' w/o the setting peacock.color
+     * and let's only check in the workspace settings.json.
+     * If there is, then let's grab it and set it.
+     */
+
+    // NOTE: If we ever remove this migration logic entirely, check if this function `getWorkspaceColorIfExists` is still be using or not, too.
+    derivedColor = getWorkspaceColorIfExists() || undefined;
+  }
+  // Migrate the color that was in the v2 memento
+  // or the derived color to the v3 workspace setting
+  const color = peacockColorMemento || derivedColor;
+  if (color) {
+    // If we found something to migrate then do it
+    Logger.info(`Migration: Applying color ${color}.`);
+    await updateColorSetting(color);
+  }
+}
