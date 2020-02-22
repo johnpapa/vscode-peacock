fcc3877325730edb286ad67c6a5981b6ebe44328
John Papa <papa@Johns-MacBook-Pro.local>
2020-02-22 09:24:25 -0500

    svelte adjustment

--- src/models/favorites.ts
+++ src/models/favorites.ts
@@ -11,6 +11,6 @@ export const starterSetOfFavorites = [
   { name: 'Node Green', value: '#215732' },
   { name: 'React Blue', value: '#61dafb' },
   { name: 'Something Different', value: '#832561' },
+  { name: 'Svelte Orange', value: '#ff3d00' },
   { name: 'Vue Green', value: '#42b883' },
-  { name: 'Svelte Orange', value: '#ff3e00' },
 ];
